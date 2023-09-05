6428eabd64f12de82bfc57e22d6e94e72d0b746e
himaloyash <himaloyashis98@gmail.com>
2023-09-05 23:48:47 +0600

    edited editor

--- src/component/Editor/Editor.jsx
+++ src/component/Editor/Editor.jsx
@@ -6,43 +6,45 @@ import 'quill/dist/quill.snow.css'
 import '@emotion/styled'
 import styled from '@emotion/styled';
 
-const component = styled.div`
-    
+const Component = styled.div`
+    background: #f5f5f5
 `
 
 const toolbarOptions = [
     ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
     ['blockquote', 'code-block'],
-  
+
     [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+    [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
     [{ 'direction': 'rtl' }],                         // text direction
-  
+
     [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
     [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-  
+
     [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
     [{ 'font': [] }],
     [{ 'align': [] }],
-  
+
     ['clean']                                         // remove formatting button
-  ];
+];
 
 const Editor = () => {
 
 
-    useEffect(()=>{
+    useEffect(() => {
 
-        const quillServer = new Quill('#container', {theme:'snow', modules: {toolbar: toolbarOptions} })
+        const quillServer = new Quill('#container', { theme: 'snow', modules: { toolbar: toolbarOptions } })
 
     }, [])
 
     return (
-        <Box id='container'>
+        <Component>
+            <Box id='container'>
 
-        </Box>
+            </Box>
+        </Component>
     );
 };
 
